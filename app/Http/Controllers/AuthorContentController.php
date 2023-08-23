5eef1b5924cf3d4acf9f3dc75b37520dbf3b9231
TohidHemmati <tohid.hemmati@jengal.com>
2023-08-23 20:18:16 +0300

    AuthorContent model created.

---
+++ app/Http/Controllers/AuthorContentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAuthorContentRequest;
+use App\Http\Requests\UpdateAuthorContentRequest;
+use App\Models\AuthorContent;
+
+class AuthorContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAuthorContentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AuthorContent $authorContent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AuthorContent $authorContent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAuthorContentRequest $request, AuthorContent $authorContent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AuthorContent $authorContent)
+    {
+        //
+    }
+}
