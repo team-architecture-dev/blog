58ab84b6d4bbe131325e00a0b5cd9f2c7a165fdf
TohidHemmati <tohid.hemmati@jengal.com>
2023-08-23 20:23:19 +0300

    Category & CategoryContent models created.

---
+++ app/Http/Controllers/CategoryContentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryContentRequest;
+use App\Http\Requests\UpdateCategoryContentRequest;
+use App\Models\CategoryContent;
+
+class CategoryContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryContentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoryContent $categoryContent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoryContent $categoryContent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryContentRequest $request, CategoryContent $categoryContent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoryContent $categoryContent)
+    {
+        //
+    }
+}
