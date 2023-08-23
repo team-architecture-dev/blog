b7fae1c96da01112a46d42ec4bc5c721cda24f9c
TohidHemmati <tohid.hemmati@jengal.com>
2023-08-23 23:09:04 +0300

    Site setting model created.

---
+++ app/Http/Controllers/SiteSettingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSiteSettingRequest;
+use App\Http\Requests\UpdateSiteSettingRequest;
+use App\Models\SiteSetting;
+
+class SiteSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSiteSettingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SiteSetting $siteSetting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SiteSetting $siteSetting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSiteSettingRequest $request, SiteSetting $siteSetting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SiteSetting $siteSetting)
+    {
+        //
+    }
+}
