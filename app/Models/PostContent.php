bcfaf79c1ee098018aaa9da2b807f608e75c7289
TohidHemmati <tohid.hemmati@jengal.com>
2023-08-23 20:20:36 +0300

    Post & PostContent models created.

---
+++ app/Models/PostContent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostContent extends Model
+{
+    use HasFactory;
+}
